dc94a290a647c969132304440e187372f9ec9bc4
GuichardTechnology <webguichard@gmail.com>
2025-01-01 12:38:54 -0300

    exo javascript

---
+++ Exo javascript.js
@@ -0,0 +1,70 @@
+                  //Exercice 1
+/*Ecrivez un programme simple qui affiche une string sur le terminal
+en utilisant le console.log.*/
+
+console.log("Hello World.");
+
+                    //Exercice 2
+/*Modifier le programme precedent pour stocker le message dans un variable Avant l afficher 
+dans la console.*/
+
+let message = "Hello World."; 
+                    
+                       //Exercice 3
+//Declarez trois variables  : nom (string), age (numero), et isStudent (boolean).
+
+let nom = "Guichard";
+let age = 30;
+let isStudent = true;
+                 
+                         //Exercice 4
+//Modifier la valeur des variables precedents et imprimer les nouvelle valeurs dans le console.
+
+let nom2 ="Jean louis";
+let age2 = 35;
+let isStudent2 = false;
+
+console.log(nom2);
+console.log(age2);
+console.log(isStudent2);
+
+                          //Exercice 5
+//Crerz une variable constante qui stockera un nombre et l imprimer dans le console.
+
+const NUMERO = 50;
+console.log(NUMERO);
+
+                              //Exercice 6
+//Declarer deux variables, adresse et telephone, sans attribuer de valeurs a chacun d eux.
+
+let adresse = undefined;
+let telephone = undefined;
+
+                                  //Exercice 7
+/*Declarez des variables avec des noms descriptifs pour stocker le nom d un produit,
+son prix, et la quantite en stock.*/
+
+let nourriture = "Riz";
+let prix = 150;
+let quantiteEnStock = 600;
+       
+                                    //Exercice 8
+/*Declarez une variable pour stocker le nom d une ville et une autre pour stocker le nom d une autre ville
+afficher un message combinant les deux variables.*/
+
+const HAITI = "port-au-prince";
+const BRESIL = "sao paulo";
+
+console.log(`${HAITI} / ${BRESIL}`);
+
+
+
+
+
+
+
+
+
+
+
+
